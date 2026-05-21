914e6c0b12f3c49aaa7421ad8fed0d5ac052a439
dev-katerina <sunshine9474379@gmail.com>
2026-05-21 11:28:33 +0300

    Репозитории для петель и инструментов

--- src/repositories/stitch_type.py
+++ src/repositories/stitch_type.py
@@ -1,55 +1,57 @@
+
+
 from abc import ABC, abstractmethod
 from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
-from models.postgres import RelationType
+from models.postgres import StitchType
 
 
-class RelationTypeRepository(ABC):
+class StitchTypeRepository(ABC):
         @abstractmethod
-        async def get_all(self) -> List[RelationType]:
+        async def get_all(self) -> List[StitchType]:
             pass
 
         @abstractmethod
-        async def get_by_name(self, name: str) -> RelationType:
+        async def get_by_name(self, name: str) -> StitchType:
             pass
 
         @abstractmethod
-        async def create(self, obj: RelationType) -> None:
+        async def create(self, obj: StitchType) -> None:
             pass
 
         @abstractmethod
-        async def update(self, obj: RelationType) -> None:
+        async def update(self, obj: StitchType) -> None:
             pass
 
         @abstractmethod
         async def delete(self, name: str) -> None:
             pass
 
-class RelationTypeRepositoryPostgres(RelationTypeRepository):
+class StitchTypeRepositoryPostgres(StitchTypeRepository):
         def __init__(self, db: AsyncSession):
             self.db = db
 
-        async def get_all(self) -> List[RelationType]:
-            result = await self.db.query(RelationType).all()
+        async def get_all(self) -> List[StitchType]:
+            result = await self.db.query(StitchType).all()
             return result
         
-        async def get_by_name(self, name: str) -> RelationType:
-            result = await self.db.select(RelationType).where(RelationType.name == name)
+        async def get_by_name(self, name: str) -> StitchType:
+            result = await self.db.select(StitchType).where(StitchType.name == name)
             return result.scalar_one_or_none()
         
-        async def create(self, obj: RelationType) -> None:
+        async def create(self, obj: StitchType) -> None:
             self.db.add(obj)
             await self.db.commit()
             await self.db.refresh(obj)
             return obj
         
-        async def update(self, obj: RelationType) -> None:
+        async def update(self, obj: StitchType) -> None:
             await self.db.commit()
             await self.db.refresh(obj)
             return obj
         
         async def delete(self, name: str) -> None:
-            result = await self.db.select(RelationType).where(RelationType.name == name)
+            result = await self.db.select(StitchType).where(StitchType.name == name)
             obj = result.scalar_one_or_none()
             if obj:
                 await self.db.delete(obj)
--- src/repositories/tool_type.py
+++ src/repositories/tool_type.py
@@ -1,55 +1,57 @@
+
+
 from abc import ABC, abstractmethod
 from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
-from models.postgres import RelationType
+from models.postgres import Tool
 
 
-class RelationTypeRepository(ABC):
+class ToolRepository(ABC):
         @abstractmethod
-        async def get_all(self) -> List[RelationType]:
+        async def get_all(self) -> List[Tool]:
             pass
 
         @abstractmethod
-        async def get_by_name(self, name: str) -> RelationType:
+        async def get_by_name(self, name: str) -> Tool:
             pass
 
         @abstractmethod
-        async def create(self, obj: RelationType) -> None:
+        async def create(self, obj: Tool) -> None:
             pass
 
         @abstractmethod
-        async def update(self, obj: RelationType) -> None:
+        async def update(self, obj: Tool) -> None:
             pass
 
         @abstractmethod
         async def delete(self, name: str) -> None:
             pass
 
-class RelationTypeRepositoryPostgres(RelationTypeRepository):
+class ToolRepositoryPostgres(ToolRepository):
         def __init__(self, db: AsyncSession):
             self.db = db
 
-        async def get_all(self) -> List[RelationType]:
-            result = await self.db.query(RelationType).all()
+        async def get_all(self) -> List[Tool]:
+            result = await self.db.query(Tool).all()
             return result
         
-        async def get_by_name(self, name: str) -> RelationType:
-            result = await self.db.select(RelationType).where(RelationType.name == name)
+        async def get_by_name(self, name: str) -> Tool:
+            result = await self.db.select(Tool).where(Tool.name == name)
             return result.scalar_one_or_none()
         
-        async def create(self, obj: RelationType) -> None:
+        async def create(self, obj: Tool) -> None:
             self.db.add(obj)
             await self.db.commit()
             await self.db.refresh(obj)
             return obj
         
-        async def update(self, obj: RelationType) -> None:
+        async def update(self, obj: Tool) -> None:
             await self.db.commit()
             await self.db.refresh(obj)
             return obj
         
         async def delete(self, name: str) -> None:
-            result = await self.db.select(RelationType).where(RelationType.name == name)
+            result = await self.db.select(Tool).where(Tool.name == name)
             obj = result.scalar_one_or_none()
             if obj:
                 await self.db.delete(obj)
